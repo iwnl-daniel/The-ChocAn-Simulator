c0ca660d4c81905d0457132f7f8d564cdf225040
Daniel Gregorio <greg25@pdx.edu>
2022-11-13 15:25:00 -0800

    finished display service function

--- registerService.py
+++ registerService.py
@@ -3,11 +3,31 @@
 # Function to display service to user
 # 
 
-def displayService(userInput: int):
-    # code to check service code is correct
+#import service dictionary
+
+# code to check service code is correct
     # return true if service matches what the user wants 
     # return false WITH error code if input doesnt match a service 
     # return false if input doesnt match a service
+def displayService(userInput: int):
+    # get userInput from service dictionary
+    # The dictionary consists of [service # : service information]
+    service = NAME_OF_SERVICE_DIRECTORY.get(userInput,0)
+    # check to see if userInput is in the dictionary 
+    if service == 0:
+        # Error message and return false
+        print("\033[31m" + "ERROR: " + "\x1b[0m" +  "No servivce corresponds to service number entered")
+        return False
+    # display service and ask user if it is correct
+    print (service)
+    user = input("Is this the correct service (y/n): ")
+    # make sure user inputs y/Y or n/N
+    while user not in ['y', 'Y', 'n', 'N']:
+        user = input("Is this the correct service (y/n): ")
+    if user in ['n', 'N']:
+        # service number is wrong and it returns False
+        return False
+    # service number is correct and it returns True
     return True
 
 
