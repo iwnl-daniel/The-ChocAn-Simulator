60050aedf8c76fac26d99aaf29e1906c5b337f65
Daniel Gregorio <greg25@pdx.edu>
2022-11-30 17:02:41 -0800

    created registerService test file and added displayService tests

---
+++ test_registerService.py
@@ -0,0 +1,59 @@
+# This file contains tests for the registerService file
+import registerService
+# === registerService function tests ===
+
+
+
+
+
+
+# === displayService function tests ===
+
+# Test: invalid user input for service number
+def test_displayService_invalid_userInput(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: "y")
+    value = registerService.displayService('123456')
+    assert value == False
+    value = registerService.displayService('213121')
+    assert value == False
+    value = registerService.displayService('918321')
+    assert value == False
+# Test: valid user input for service number
+def test_displayService_valid_userInput(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: "y")
+    value = registerService.displayService('175932')
+    assert value == True
+    value = registerService.displayService('741960')
+    assert value == True
+    value = registerService.displayService('583667')
+    assert value == True
+# Test: user can enter either y, Y, n, or N as inputs when 
+# asked if service number matches 
+def test_userInput_correct_service_question(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: "y")
+    value = registerService.displayService('175932')
+    assert value == True
+    monkeypatch.setattr('builtins.input', lambda _: "Y")
+    value = registerService.displayService('175932')
+    assert value == True
+    monkeypatch.setattr('builtins.input', lambda _: "n")
+    value = registerService.displayService('175932')
+    assert value == False
+    monkeypatch.setattr('builtins.input', lambda _: "N")
+    value = registerService.displayService('175932')
+    assert value == False
+
+  
+
+# Test 
+
+# Test 
+
+# Test 
+
+# Test 
+
+
+
+
+
