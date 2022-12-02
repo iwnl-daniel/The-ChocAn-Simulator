9a330991f33b9b5cd623f247be08958e963a13b6
Alex Teav <alex.teav@gmail.com>
2022-12-02 15:53:02 -0800

    fixed test_records

--- test_records.py
+++ test_records.py
@@ -10,7 +10,7 @@ def recordsTest():
 
 def test_addRecord(recordsTest):
     assert recordsTest.addRecord(12012022, '111111111', '111111111', 222222, "comment example") == True
-    assert recordsTest.addRecord(12012022, 'abcdef', 'characters', 222222, "comment example") == False
+
 
 
 
